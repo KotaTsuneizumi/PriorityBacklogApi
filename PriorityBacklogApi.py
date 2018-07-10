694408b9c6a56482aa3e2c640d145306b640667f
tsuneizumik <tsuneizumik@nttdata.co.jp>
2018-07-11 03:08:57 +0900

    requestsによるAPI発行部分を更新（まだうまく動かないけど

--- PriorityBacklogApi.py
+++ PriorityBacklogApi.py
@@ -1,1 +1,15 @@
-print("Hello World!")
+# -*- coding: utf-8 -*-
+
+import json
+import requests
+
+url = 'https://kotatsu.backlog.jp/api/v2/issues?apiKey=JGKbqgXYOWuJBwKmnq023EnFdiHVG7kIzziEu1xhoIeW02kKJXHVgzLd9fvX1J13'
+
+query = {
+        'projectId': '22965', #プロジェクトのID
+        }
+
+r = requests.get(url)
+
+print(r)
+print(json.dumps(r.json(), sort_keys=True, indent=2))
